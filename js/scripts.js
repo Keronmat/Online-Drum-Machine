3a3a7e122523d24e41aec6f36746b7a62bfbc48e
Keron Matthews <keronmat@live.com>
2019-02-19 15:59:13 -0400

    color changes when on and off

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
+const checkbox = document.getElementById("cmn-toggle-1");
+
+checkbox.addEventListener("change", function() {
+  const mainDrum = document.querySelector("#main-drum");
+  if (this.checked) {
+    mainDrum.classList.remove("main-drumOff");
+    mainDrum.classList.add("main-drumOn");
+    //document.querySelectorAll("kbd").classList.add("kbd-on");
+  } else {
+    mainDrum.classList.remove("main-drumOn");
+    mainDrum.classList.add("main-drumOff");
+  }
+});
+
 function removeTransition(e) {
   if (e.propertyName !== "transform") return;
   e.target.classList.remove("playing");
@@ -6,7 +20,7 @@ function removeTransition(e) {
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.drum-key[data-key="${e.keyCode}"]`);
-  const checkbox = document.getElementById("cmn-toggle-1");
+
   if (checkbox.checked === true) {
     if (!audio) return;
     key.classList.add("playing");
@@ -20,14 +34,16 @@ keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 //window.addEventListener("keydown", playSound);
 
 //trying clicks
-
 keys.forEach(el => {
   el.addEventListener("click", () => {
     const audioKey = el.getAttribute("data-key");
     const audio = document.querySelector(`audio[data-key="${audioKey}"]`);
-    el.classList.add("playing");
-    audio.currentTime = 0;
-    audio.play();
+    if (checkbox.checked === true) {
+      el.classList.add("playing");
+      audio.currentTime = 0;
+      audio.play();
+    } else return alert("Please click the On button!"); // ensure the button is on
+    turn;
   });
 });
 
